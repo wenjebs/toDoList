07b6aab7a670a250cc2a6d275d38c8fce844cb23
wenjie <wenjieb2@gmail.com>
2022-10-17 22:14:02 +0800

    Basic setup

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  
 };
