ddb0f2bd6f431f6fd31f3da5a93e3e799b0d9fe1
wenjie <wenjieb2@gmail.com>
2022-10-18 14:18:01 +0800

    Implement popup for add task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import './style.css';
 
 const addTaskButton = document.querySelector('.add-task');
+const addTaskPopup = document.querySelector('.bg-modal');
+const addTaskClose = document.querySelector('.close-popup');
 
 addTaskButton.addEventListener('click', ()=>{
-    document.body.textContent = '';
-})
+    addTaskPopup.style.display = 'flex';
+});
+
+addTaskClose.addEventListener('click', ()=> {
+    addTaskPopup.style.display = 'none';
+});
