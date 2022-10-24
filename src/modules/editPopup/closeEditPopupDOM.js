9ecf3bf03fbcf6b567e06a35d491b07433af51fb
wenjie <wenjieb2@gmail.com>
2022-10-24 14:10:29 +0800

    clear files for refactor

--- src/modules/editPopup/closeEditPopupDOM.js
+++
@@ -1,9 +0,0 @@
-function closeEditPopup() {
-    document.addEventListener('click',(e)=>{
-        if (e.target.classList.contains('close-edit-popup')) {
-            e.target.parentNode.parentNode.parentNode.remove();
-        } 
-    })
-};
-
-export default closeEditPopup
