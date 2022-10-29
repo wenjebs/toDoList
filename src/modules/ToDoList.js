91102ad0f888084d9466c693aaa4531a57482414
wenjie <wenjieb2@gmail.com>
2022-10-29 21:07:52 +0800

    Allow for fxn modularity

---
+++ src/modules/ToDoList.js
@@ -0,0 +1,19 @@
+class ToDoList {
+    constructor() {
+        this.projects = []
+    }
+
+    getProject(projectTitle) {
+        return this.projects.find((project)=>project.getTitle() === projectTitle);
+    }
+
+    getProjects() {
+        return this.projects
+    }
+
+    addProject(project) {
+        this.projects.push(project)
+    }
+}
+
+export default ToDoList;
